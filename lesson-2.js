85b8c1ad213f6c80be3a5f8ddc6f03cb71bd95ed
Avi <antonovvit@gmail.com>
2023-11-26 23:23:39 +0400

    import tree

--- lesson-2.js
+++ lesson-2.js
@@ -1,27 +1,9 @@
 import {Command} from "commander";
 import {readdir} from "node:fs/promises"
 import path from "path";
+import {tree} from "./tree.js";
 
 const app = new Command();
-
-
-function tree(input) {
-    const traverse = ({name, items}) =>
-        name +
-        (items?.length ?
-            '\n' + items
-                .map(traverse)
-                .map((childText, childIndex, {length}) =>
-                    childIndex < length - 1
-                        ? '├── ' + childText.replace(/\n/g, '\n│   ')
-                        : '└── ' + childText.replace(/\n/g, '\n    ')
-                )
-                .join('\n')
-            : '')
-
-
-    console.log(traverse(input))
-}
 
 app.version('0.0.1')
 app.command('tree <startPath>')
